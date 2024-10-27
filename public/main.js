c5f535077497f406acf600e4a4f9ac72e042957b
Oliver Hughes <oliver.hughes@adevinta.com>
2024-10-27 17:00:07 +0100

    adding mobile docs menu

--- public/main.js
+++ public/main.js
@@ -1,30 +1,39 @@
 (function NavToggle() {
-  const navBtn = document.getElementById("nav-toggle");
-  const navMenu = document.getElementById("nav-menu");
-  const navLinks = navMenu.querySelectorAll("a");
-  let expanded = false;
+  addToggleListener("nav-toggle", "nav-menu");
+  addToggleListener("docs-nav-mobile-toggle", "docs-nav-mobile-menu");
 
-  navBtn.addEventListener("click", () => {
-    toggleNav();
-  });
+  function addToggleListener(btnId, navId) {
+    const navBtn = document.getElementById(btnId);
+    const navMenu = document.getElementById(navId);
+    if (!navBtn || !navMenu) {
+      return;
+    }
 
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
+    const navLinks = navMenu.querySelectorAll("a");
+    let expanded = false;
+
+    navBtn.addEventListener("click", () => {
       toggleNav();
     });
-  });
-
-  function toggleNav() {
-    expanded = !expanded;
-    navMenu.classList.toggle("hidden");
-    navMenu.classList.toggle("flex");
-    navMenu.setAttribute("aria-hidden", !expanded);
-    navBtn.setAttribute("aria-expanded", expanded);
-    navBtn.classList.toggle("rotate-90");
 
     navLinks.forEach((link) => {
-      link.tabIndex = expanded ? 0 : -1;
+      link.addEventListener("click", () => {
+        toggleNav();
+      });
     });
-    if (expanded) navLinks[0].focus();
+
+    function toggleNav() {
+      expanded = !expanded;
+      navMenu.classList.toggle("hidden");
+      navMenu.classList.toggle("block");
+      navMenu.setAttribute("aria-hidden", !expanded);
+      navBtn.setAttribute("aria-expanded", expanded);
+      navBtn.classList.toggle("rotate-90");
+
+      navLinks.forEach((link) => {
+        link.tabIndex = expanded ? 0 : -1;
+      });
+      if (expanded) navLinks[0].focus();
+    }
   }
 })();
