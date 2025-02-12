cfaa71b346a8a5835752fbd8bd5a175b6f333e04
Oliver Hughes <hughes.oliver88@gmail.com>
2025-02-12 17:53:19 +0100

    add fade in on scroll

--- public/main.js
+++ public/main.js
@@ -34,3 +34,34 @@
     }
   }
 })();
+
+(function RegisterFadeInElements() {
+  const elements = document.getElementsByClassName("element-fade-in");
+  if (elements.length === 0) return;
+  const intersectionObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry, i) => {
+        const element = entry.target;
+        if (!entry.isIntersecting && !element.dataset.animated) {
+          element.classList.add("opacity-10");
+          element.classList.add("-translate-y-10");
+          return;
+        }
+        element.dataset.animated = "true";
+        setTimeout(() => {
+          element.classList.remove("opacity-10");
+          element.classList.remove("-translate-y-10");
+        }, i * 150);
+      });
+    },
+    {
+      root: null,
+      threshold: 0.1,
+    },
+  );
+  Array.from(elements).forEach((element) => {
+    element.classList.add("transition");
+    element.classList.add("duration-700");
+    intersectionObserver.observe(element);
+  });
+})();
